c2e792596cf7ba8429b68f9ac13f95106cb23c4f
ProgrammerE <ProgrammerE@users.noreply.github.com>
2023-04-12 11:08:35 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 
 function callApi() {
+  alert('Button was pressed!');
   fetch('https://postman-echo.com/get')
     .then(data => data.json()) // Parsing the data into a JavaScript object
     .then(json => alert(JSON.stringify(json))) // Displaying the stringified data in an alert popup
