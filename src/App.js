fdd0c524139c2b334e38198407e7db938447c1fc
ProgrammerE <ProgrammerE@users.noreply.github.com>
2023-04-12 10:44:47 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch('https://odd-teal-hummingbird-gear.cyclic.app/books')
+      .then(response => response.json())
+      .then(data => setData(data))
+      .catch(error => console.error(error));
+  }, []);
 
   return (
-    
-    fetch('https://odd-teal-hummingbird-gear.cyclic.app/books', { method: 'GET' })
-      .then(data => data.json())
-      .then(json => alert(JSON.stringify(json)))
-    
+    <div>
+      {data ? (
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
   );
 }
 export default App;
