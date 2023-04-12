5849654fa61cb084e220be1447657ea772095792
ProgrammerE <ProgrammerE@users.noreply.github.com>
2023-04-12 11:00:42 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 
 function callApi() {
-  fetch('postman-echo.com/get', { method: 'GET' })
+  fetch('postman-echo.com/get')
     .then(data => data.json()) // Parsing the data into a JavaScript object
     .then(json => alert(JSON.stringify(json))) // Displaying the stringified data in an alert popup
 }
