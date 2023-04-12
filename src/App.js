e46cb69d5d69550a85eced9bec1982506a2bab38
ProgrammerE <ProgrammerE@users.noreply.github.com>
2023-04-12 10:51:56 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
+import React from 'react';
 import './App.css';
-import React, { useState, useEffect } from 'react';
+
+function callApi() {
+  alert('Button was pressed!');
+}
 
 function App() {
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    fetch('https://odd-teal-hummingbird-gear.cyclic.app/books')
-      .then(response => response.json())
-      .then(data => setData(data))
-      .catch(error => console.error(error));
-  }, []);
-
   return (
-    <div>
-      {data ? (
-        <pre>{JSON.stringify(data, null, 2)}</pre>
-      ) : (
-        <p>Loading...</p>
-      )}
+    <div className="App">
+      <header className="App-header">
+        <button onClick={callApi}>Call API</button>
+      </header>
     </div>
   );
 }
+
 export default App;
