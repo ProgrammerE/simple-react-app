f55fd6024368bd9ef4a528dd8e5047fa8fa07899
ProgrammerE <ProgrammerE@users.noreply.github.com>
2023-04-12 10:19:45 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [books, setData] = useState(null);
 
   useEffect(() => {
-    fetch('https://odd-teal-hummingbird-gear.cyclic.app/books')
+    fetch('https://odd-teal-hummingbird-gear.cyclic.app/books', { method: 'GET' })
       .then(response => response.json())
       .then(books => setData(books))
       .catch(error => console.error(error));
