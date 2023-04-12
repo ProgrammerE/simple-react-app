2d27a0781802caf21c16c9f20f64ef747a96b416
ProgrammerE <ProgrammerE@users.noreply.github.com>
2023-04-12 11:33:46 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 
 function callApi() {
-  alert('Button was pressed!');
   fetch('https://postman-echo.com/get', { method: 'GET' })
     .then(response => response.json())
     .then(data => alert(data.title) ) // Displays the firstName from the API response
